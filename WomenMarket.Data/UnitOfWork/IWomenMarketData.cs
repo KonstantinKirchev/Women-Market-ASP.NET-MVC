360cbcdaff0af6a0075a1d7de5568fe9328d5f2e
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-03-14 18:30:51 +0200

    Repositories and Unit of work are ready

---
+++ WomenMarket.Data/UnitOfWork/IWomenMarketData.cs
@@ -0,0 +1,20 @@
+﻿namespace WomenMarket.Data.UnitOfWork
+{
+    using Repositories;
+    using Models;
+
+    public interface IWomenMarketData
+    {
+        IRepository<User> Users { get; }
+
+        IRepository<Category> Categories { get; }
+
+        IRepository<Farm> Farms { get; }
+
+        IRepository<Product> Products { get; }
+
+        IRepository<ShoppingCart> ShoppingCarts { get; }
+
+        int SaveChanges();
+    }
+}
---
+++ WomenMarket.Data/UnitOfWork/WomenMarketData.cs
@@ -0,0 +1,91 @@
+﻿namespace WomenMarket.Data.UnitOfWork
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Repositories;
+    using Models;
+
+    public class WomenMarketData : IWomenMarketData
+    {
+        private readonly DbContext dbContext;
+
+        private readonly IDictionary<Type, object> repositories;
+
+        private IUserStore<User> userStore;
+
+        public WomenMarketData(DbContext context)
+        {
+            this.dbContext = context;
+            this.repositories = new Dictionary<Type, object>();
+        }
+        public IRepository<User> Users
+        {
+            get
+            {
+                return this.GetRepository<User>();
+            }
+        }
+
+        public IRepository<Category> Categories
+        {
+            get
+            {
+                return this.GetRepository<Category>();
+            }
+        }
+        public IRepository<Product> Products
+        {
+            get
+            {
+                return this.GetRepository<Product>();
+            }
+        }
+
+        public IRepository<Farm> Farms
+        {
+            get
+            {
+                return this.GetRepository<Farm>();
+            }
+        }
+
+        public IRepository<ShoppingCart> ShoppingCarts
+        {
+            get { return this.GetRepository<ShoppingCart>(); }
+        }
+
+        public IUserStore<User> UserStore
+        {
+            get
+            {
+                if (this.userStore == null)
+                {
+                    this.userStore = new UserStore<User>(this.dbContext);
+                }
+
+                return this.userStore;
+            }
+        }
+
+        public int SaveChanges()
+        {
+            return this.dbContext.SaveChanges();
+        }
+
+        private IRepository<T> GetRepository<T>() where T : class
+        {
+            if (!this.repositories.ContainsKey(typeof(T)))
+            {
+                var type = typeof(GenericEfRepository<T>);
+                this.repositories.Add(
+                    typeof(T),
+                    Activator.CreateInstance(type, this.dbContext));
+            }
+
+            return (IRepository<T>)this.repositories[typeof(T)];
+        }
+    }
+}
