f28d0a95726effb83b8eb3ae84cd6c8258b36430
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-03-15 22:41:42 +0200

    Create new HtmlHelper class with extension helpers which I use in the views.

---
+++ WomenMarket.App/Extensions/HtmlHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace WomenMarket.App.Extensions
+{
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString EmailTextBox(this HtmlHelper helper, string email)
+        {
+            TagBuilder builder = new TagBuilder("a");
+            builder.MergeAttribute("href", $"mailto: {email}");
+            builder.SetInnerText(email);
+            return new MvcHtmlString(builder.ToString(TagRenderMode.Normal));
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string imgUrl, string alt, string style)
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.AddCssClass("img-responsive");
+            builder.MergeAttribute("src", imgUrl);
+            builder.MergeAttribute("alt", alt);
+            builder.MergeAttribute("style", style);
+            return new MvcHtmlString(builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+        public static MvcHtmlString HyperLink(this HtmlHelper helper, string link, string icon)
+        {
+            TagBuilder builder = new TagBuilder("a");
+            builder.MergeAttribute("href", link);
+            builder.MergeAttribute("target", "_blank");
+            TagBuilder builderIcon = new TagBuilder("i");
+            builderIcon.AddCssClass(icon);
+            builder.SetInnerText(builderIcon.ToString());
+            return new MvcHtmlString(builder.ToString());
+        }
+    }
+}
