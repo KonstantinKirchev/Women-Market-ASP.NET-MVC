17406e4ce6c6d1c422b41cd43375da07263d0bcd
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-05-04 23:27:26 +0300

    Added custom error handling (404 and 500).

---
+++ WomenMarket.App/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿namespace WomenMarket.App.Controllers
+{
+    using System.Web.Mvc;
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ViewResult Error404()
+        {
+            Response.StatusCode = 404; 
+            return View();
+        }
+
+        public ViewResult Error500()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
